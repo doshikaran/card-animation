b8b003e1e8fb84fba47ea947c4973eb8887c513e
doshikaran <doshikaran59@gmail.com>
2022-11-18 17:22:44 -0800

    first commit

---
+++ .history/app_20221118171100.js
@@ -0,0 +1,18 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+container.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  });
+
+  container.addEventListener("mouseenter", (e) => {
+    card.style.transition = "none";
+    //Popout
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(75px)";
+  });
